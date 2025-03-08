e26f8624d56f580022a25f589e3e440f93176285
Nicolas-Tworeck <nico.tworeck@gmail.com>
2025-03-08 20:22:33 -0300

    commit

--- script/href.js
+++ script/href.js
@@ -4,6 +4,9 @@ function filosofia(){
 function processo(){
     window.location.href = "#processo";
 }
+function planos(){
+    window.location.href = "#planos";
+}
 function nav(){
     window.location.href = "#navbar";
 }
