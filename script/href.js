7cc0059999aa147f98c5dd9fa68999a670f412c5
Nicolas-Tworeck <nico.tworeck@gmail.com>
2025-04-21 14:09:49 -0300

    commit

--- script/href.js
+++ script/href.js
@@ -21,8 +21,8 @@ function whatsNd() {
     window.location.href = "https://api.whatsapp.com/send?phone=5542984033886&text=Ol%C3%A1%2C%20vim%20pelo%20site%20da%20CrealityTech%2C%20gostei%20muito%20dos%20trabalhos%2C%20gostaria%20de%20or%C3%A7ar%20um%20servi%C3%A7o.";
 }
 function instaCreality(){
-    window.location.href = "https://www.instagram.com/creality_tech?igsh=a2d5OXUybnRpYjRm";
+    window.location.href = "https://www.instagram.com/creality__tech/?utm_source=ig_web_button_share_sheet";
 }
 function mail(){
-    window.location.href = "mailto:contato@crealitytech.com.br";
+    window.location.href = "mailto:central@crealitytech.com.br";
 }
